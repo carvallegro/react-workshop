8518f47892e53393239ae2919d3ad0ab83f892a5
Valentin <carvallegro@users.noreply.github.com>
2021-11-11 15:34:55 +1100

    feat: Created basic user table

---
+++ src/features/users/usertable/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Dropdown, Trigger, Menu, Item } from '@zendeskgarden/react-dropdowns';
+import {
+    Body,
+    Cell,
+    Head,
+    HeaderCell,
+    HeaderRow,
+    OverflowButton,
+    Row,
+    Table
+} from '@zendeskgarden/react-tables';
+
+import data from './MOCK_DATA.json'
+
+const OverflowMenu = () => (
+    <Dropdown>
+        <Trigger>
+            <OverflowButton aria-label="Row actions" />
+        </Trigger>
+        <Menu
+            placement="bottom-end"
+            popperModifiers={{
+                preventOverflow: {
+                    boundariesElement: 'viewport'
+                },
+                flip: {
+                    enabled: false
+                },
+                offset: {
+                    fn: data => {
+                        /**
+                         * Ensure correct placement relative to trigger
+                         **/
+                        data.offsets.popper.top -= 2;
+
+                        return data;
+                    }
+                }
+            }}
+        >
+            <Item value="item-1" disabled>Profile</Item>
+        </Menu>
+    </Dropdown>
+);
+
+/**
+ * TODO:
+ * 1. map the users props to the Garden table
+ * 2. (Optional) Add an empty state.
+ *
+ * @param users
+ * @return {JSX.Element}
+ * @constructor
+ */
+export const UserTable = ({users = []}) => {
+
+    return (
+        <div style={{overflowX: 'auto'}}>
+            <Table style={{minWidth: 500}}>
+                <Head>
+                    <HeaderRow>
+                        <HeaderCell>Id</HeaderCell>
+                        <HeaderCell>Name (username)</HeaderCell>
+                        <HeaderCell>email</HeaderCell>
+                        <HeaderCell>website</HeaderCell>
+                        <HeaderCell hasOverflow>
+                            <OverflowMenu/>
+                        </HeaderCell>
+                    </HeaderRow>
+                </Head>
+                <Body>
+                    <Row>
+                        <Cell>id</Cell>
+                        <Cell>name (username)</Cell>
+                        <Cell>email</Cell>
+                        <Cell>website</Cell>
+                        <Cell hasOverflow>
+                            <OverflowMenu/>
+                        </Cell>
+                    </Row>
+                </Body>
+            </Table>
+        </div>
+    );
+};
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import {UserTable} from "../features/users/usertable";
+import { XXXL } from '@zendeskgarden/react-typography';
 
-export const UsersPage = () => <div>Users Page</div>
+/**
+ * TODO:
+ * 1. fetch the users
+ * 2. show a loading state
+ * (Optional) 3. Display error state
+ * (Optional) 4. Add a button to refresh the data
+ * (Optional) 5. Add automatic refresh every 10 sec
+ * @return {JSX.Element}
+ * @constructor
+ */
+export const UsersPage = () => <div>
+    <XXXL tag='h1'>List of users</XXXL>
+    <UserTable users={[]} />
+</div>
