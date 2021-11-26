9519b9550407155d2d05880ee75720f46c12fc55
Linda Lai <llai@zendesk.com>
2021-11-26 13:40:21 +1100

    test: Add theme provider wrapper to Users tests

--- src/pages/Users.test.js
+++ src/pages/Users.test.js
@@ -1,35 +1,49 @@
-import {render, fireEvent, waitFor, screen} from '@testing-library/react'
-import TEST_DATA from './TEST_DATA.json'
-import {UsersPage} from "./Users";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import TEST_DATA from "./TEST_DATA.json";
+import { UsersPage } from "./Users";
+import { ThemeProvider, DEFAULT_THEME } from "@zendeskgarden/react-theming";
 
 // TODO: Add THemeProvider wrapper
-describe('<UsersPage />', () => {
-    beforeAll(() => {
-        jest.spyOn(window, 'fetch');
-    })
+describe("<UsersPage />", () => {
+  beforeAll(() => {
+    jest.spyOn(window, "fetch");
+  });
 
-    describe('render', () => {
-        beforeEach(() => {
-            window.fetch.mockResolvedValue({
-                ok: true,
-                json: async () => new Promise((resolve) => setTimeout(() => resolve(TEST_DATA), 500))
-            })
-        })
-
-        it('should render a loading state', async () => {
-            render(<UsersPage />)
-
-            await waitFor(() => screen.getByRole('heading', {level: 2}))
-
-            expect(screen.getByRole('heading', {level: 2})).toHaveTextContent('Users are loading...')
-        })
-
-        it('should render two lines in the table', async () => {
-            render(<UsersPage />)
-
-            await waitFor(() => screen.getAllByRole('row'))
-
-            expect(screen.getAllByRole('row')).toHaveLength(3)
-        })
+  describe("render", () => {
+    beforeEach(() => {
+      window.fetch.mockResolvedValue({
+        ok: true,
+        json: async () =>
+          new Promise((resolve) => setTimeout(() => resolve(TEST_DATA), 500)),
+      });
     });
+
+    it("should render a loading state", async () => {
+      render(<UsersPage />, {
+        wrapper: (props) => (
+          <ThemeProvider
+            {...props}
+            focusVisibleRef={null}
+            theme={DEFAULT_THEME}
+          />
+        ),
+      });
+
+      await waitFor(() => screen.getByRole("heading", { level: 2 }));
+
+      expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+        "Users are loading..."
+      );
+    });
+
+    it("should render two lines in the table", async () => {
+      render(<UsersPage />, {
+        wrapper: (props) => <ThemeProvider {...props} />,
+      });
+
+      await waitFor(() => screen.getAllByRole("row"));
+
+      expect(screen.getAllByRole("row")).toHaveLength(3);
+    });
+  });
 });
