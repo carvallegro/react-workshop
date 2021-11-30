6cd56f21e559d3dd6d197b6875501eb91ed078f1
Linda Lai <llai@zendesk.com>
2021-11-30 13:59:37 +1100

    Add filters param to useFetchUsers to construct fetch endpoint

--- src/features/users/hooks.js
+++ src/features/users/hooks.js
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useFetchUsers = () => {
+export const useFetchUsers = (filters = {}) => {
   const [isLoading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
+  const queryString = Object.entries(filters)
+    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+    .join("&");
+
+  let url = "https://jsonplaceholder.typicode.com/users`";
+
+  if (queryString) url = `${url}?${queryString}`;
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch(url)
       .then((result) => result.json())
       .then((users) => setUsers(users))
       .then(() => setLoading(false));
