0d4a10a8e745939b8d50be156b55f06ce0af48e5
Linda Lai <llai@zendesk.com>
2021-11-30 13:02:59 +1100

    Setup User component for exercise two

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,15 +1,83 @@
 import React from "react";
+
 import { PALETTE } from "@zendeskgarden/react-theming";
 import { Avatar } from "@zendeskgarden/react-avatars";
-import { Row, Col } from "@zendeskgarden/react-grid";
 import { ReactComponent as UserIcon } from "@zendeskgarden/svg-icons/src/16/user-solo-stroke.svg";
+import { Row, Col } from "@zendeskgarden/react-grid";
+import { Timeline } from "@zendeskgarden/react-accordions";
+import { Span } from "@zendeskgarden/react-typography";
+
+import { Well } from "@zendeskgarden/react-notifications";
 
 export const UserPage = () => (
-  <Row>
-    <Col textAlign="center">
+  <>
+    {/* <Row>
+      <Col textAlign="center"></Col>
       <Avatar backgroundColor={PALETTE.grey[600]}>
-        <Avatar.Text>ZD</Avatar.Text>
+        <UserIcon role="img" aria-label="icon avatar" />
       </Avatar>
-    </Col>
-  </Row>
+    </Row> */}
+
+    <Row justifyContent="center">
+      <Col sm="auto">
+        <Timeline>
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">Profile</Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Avatar backgroundColor={PALETTE.grey[600]}>
+                <UserIcon role="img" aria-label="icon avatar" />
+              </Avatar>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">ID</Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Span isBold>???</Span>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">Name</Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Span isBold>???</Span>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">Username</Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Span isBold>???</Span>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">Website</Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Span isBold>???</Span>
+            </Timeline.Content>
+          </Timeline.Item>
+
+          <Timeline.Item>
+            <Timeline.OppositeContent>
+              <Span hue="grey">Catch Phrase </Span>
+            </Timeline.OppositeContent>
+            <Timeline.Content>
+              <Span isBold>"Multi-layered client-server neural-net"</Span>
+            </Timeline.Content>
+          </Timeline.Item>
+        </Timeline>
+      </Col>
+    </Row>
+  </>
 );
