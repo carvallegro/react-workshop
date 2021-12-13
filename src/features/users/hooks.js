0ff14603432dcbd9a6579aabe6ed4dd0d62de6a4
Valentin <carvallegro@users.noreply.github.com>
2021-12-13 13:27:23 +1100

    chore: Added exercise instructions

--- src/features/users/hooks.js
+++ src/features/users/hooks.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+/*
+ * TODO: error handling
+ *  - generic
+ */
+// 1. Generic error handling
+// 2. (Optional) User not found
+// 3. (Optional) Server error
 export const useFetchUsers = (filters = {}) => {
   const [isLoading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
